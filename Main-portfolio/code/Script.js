362d14b2141faacc1985dfa3436435626a1a1c5a
Nandyala Sri Manikanta Subbarao <srimaninandyala.com>
2024-07-25 11:08:33 +0530

    smooth-scrolling

--- Main-portfolio/code/Script.js
+++ Main-portfolio/code/Script.js
@@ -1,4 +1,33 @@
-const vi = confirm(
-  "Eventlistners and smooth scrolling not added yet.buttons not gona work for now"
-);
-console.log(vi);
+// selectors
+const nav_home = document.querySelector(".nav-home");
+const nav_about = document.querySelector(".nav-About");
+const nav_skill = document.querySelector(".nav-skills");
+
+const main_text = document.querySelector(".main-text");
+const nav_projects = document.querySelector(".nav-projects");
+const skill_about = document.querySelector(".about-skills");
+const skill_show = document.querySelector(".skills");
+const projects = document.querySelector(".Projects");
+const bvc_eduacion_select = document.getElementById("bvc_eduaction");
+const bvc_educationn = document.querySelector(".education");
+
+// EventListners
+nav_home.addEventListener("click", function (e) {
+  main_text.scrollIntoView({ behavior: "smooth" });
+});
+
+nav_about.addEventListener("click", function (e) {
+  skill_about.scrollIntoView({ behavior: "smooth" });
+});
+
+nav_skill.addEventListener("click", function (e) {
+  skill_show.scrollIntoView({ behavior: "smooth" });
+});
+
+nav_projects.addEventListener("click", function (e) {
+  projects.scrollIntoView({ behavior: "smooth" });
+});
+
+bvc_eduacion_select.addEventListener("click", function (e) {
+  bvc_educationn.scrollIntoView({ behavior: "smooth" });
+});
